b8e96e52bbbbf224577c5577b5ae396eaac899d3
sneideraitism <sneideraitism@IAL13855.ial.intra>
2024-10-30 14:26:07 +0100

    commit3

---
+++ Project1/src/Project1/WochenAblauf.java
@@ -0,0 +1,37 @@
+package Project1;
+
+//Aufgabe:
+//
+//Erstellen Sie eine Klasse Wochenablauf, die per Switch-Case in der Konsole ausgibt was erledigt werden muss:
+//
+//Montag: Einkaufen
+//
+//Dienstag: Fortbildung
+//
+//Donnerstag: Sport
+//
+//Freitag: Party
+//
+//Sonntag: Freunde treffen
+//
+//Wenn nichts geplant ist dann wird sich um den Haushalt gekümmert. 
+
+public class WochenAblauf {
+	public static void main(String[] args) {
+		String tag = "Montag";
+		switch (tag) {
+		case "Montag": System.out.println("Einkaufen");
+			break;
+		case "Dienstag": System.out.println("Fortbildung");
+			break;
+		case "Donnerstag": System.out.println("Sport");
+			break;
+		case "Freitag": System.out.println("Party");
+			break;
+		case "Sonntag": System.out.println("Freunde treffen");
+			break;
+		default: System.out.println("um den Haushalt kümmern");
+			break;
+		} 
+	}
+}
