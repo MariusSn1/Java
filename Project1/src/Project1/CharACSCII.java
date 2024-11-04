5477f7f8dbca34f4ba4ee31a4645282cedd18757
sneideraitism <sneideraitism@IAL13855.ial.intra>
2024-11-04 09:09:12 +0100

    04.11.2024 09:09

--- Project1/src/Project1/CharACSCII.java
+++ Project1/src/Project1/CharACSCII.java
@@ -4,7 +4,13 @@ public class CharACSCII {
 
 	public static void main(String[] args) {
 		char symbol = 'Ö';
-		System.out.print("Die ASCII Codierung für das Zeichen " + symbol + " ist: " + (int) symbol);
+		System.out.println("Die ASCII Codierung für das Zeichen " + symbol + " ist: " + (int)symbol);
+		long a = 263255215489711115l;
+		byte b = 107;
+		double c = 17894561461899741185498487.61497541793145925893974563275823143972174219743297512349723197432957297453217932975429731297429725975327492797459239754297;
+		float d = (float)c;
+		System.out.println(a + " " + b + "\t\t" + c + "\n\t\t\t\t" + d);
+		System.out.println("\t" + 1.7E+3);
 	}
 
 }
