2eef22405daec6ccfb7b4e2f359d99f15d3e85b5
sneideraitism <sneideraitism@IAL13855.ial.intra>
2024-11-04 12:05:49 +0100

    04.11.2024 12:05

--- Project1/src/Project1/Temperatur.java
+++ Project1/src/Project1/Temperatur.java
@@ -18,31 +18,33 @@ public class Temperatur {
 	
 	public static void main(String[] args) {
 		
-		int monat = 7;
+		int monat = 12;
 		String jahresZeit;
-		double temperatur = 15.7;
+		String temperaturEmpfinden;
+		double temperatur = 10.7;
 		switch (monat) {
 			case 3, 4, 5:
 				jahresZeit = "Frühling";
-				System.out.println("Der " + monat + ". Monat ist mit " + temperatur + "°C für " + jahresZeit + " relativ " + ((temperatur >= 20) ? "warm" : "kalt"));
+				temperaturEmpfinden = (temperatur >= 20) ? "warm" : "kalt";
 				break;
 			case 6, 7, 8:
 				jahresZeit = "Sommer";
-				System.out.println("Der " + monat + ". Monat ist mit " + temperatur + "°C für " + jahresZeit + " relativ " + ((temperatur >= 25) ? "warm" : "kalt"));	
+				temperaturEmpfinden = (temperatur >= 25) ? "warm" : "kalt";
 				break;
 			case 9, 10, 11:
 				jahresZeit = "Herbst";
-				System.out.println("Der " + monat + ". Monat ist mit " + temperatur + "°C für " + jahresZeit + " relativ " + ((temperatur >= 15) ? "warm" : "kalt"));
+				temperaturEmpfinden = (temperatur >= 15) ? "warm" : "kalt";
 				break;
 			case 12, 1, 2:
 				jahresZeit = "Winter";
-				System.out.println("Der " + monat + ". Monat ist mit " + temperatur + "°C für " + jahresZeit + " relativ " + ((temperatur >= 5) ? "warm" : "kalt"));
+				temperaturEmpfinden = (temperatur >= 5) ? "warm" : "kalt";
 				break;
 			default:
 				jahresZeit = "ungültig";
-				System.out.println(monat + " ist kein gültiger Monat");
+				temperaturEmpfinden = "nicht verfügbar";
 				break;
-		}		
+		}
+		System.out.println((jahresZeit != "ungültig") ? ("Der " + monat + ". Monat ist mit " + temperatur + "°C für " + jahresZeit + " relativ " + temperaturEmpfinden) : (monat + " ist kein gültiger Monat"));
 		
 	}
 }
