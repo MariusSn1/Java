209e0b95468daeeb3669af444bddec6f7fb74d2b
sneideraitism <sneideraitism@IAL13855.ial.intra>
2024-11-04 14:23:55 +0100

    04.11.2024 14:23

---
+++ Project1/src/Project1/WhileSchleife.java
@@ -0,0 +1,31 @@
+package Project1;
+
+/*Mithile der While-Schleife geben Sie folgeden Zahlenstränge aus:
+    0, 1, 2, 3, …, 10
+    -10, -8, -6, …, +10
+    1, 2, 4, 8, … bis 512
+ */
+
+public class WhileSchleife {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i <= 10) {
+			System.out.print(i + " ");
+			i++;
+		}
+		System.out.print("\n");
+		i = -10;
+		while (i <= 10) {
+			System.out.print(i + " ");
+			i++;
+		}
+		System.out.print("\n");
+		i = 1;
+		while (i <= 512) {
+			System.out.print(i + " ");
+			i = i*2;
+		}
+	}
+
+}
