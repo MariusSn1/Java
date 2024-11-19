dd4c8b3cf33a4c8da55a671901a8f2485c2ebd33
sneideraitism <sneideraitism@IAL13855.ial.intra>
2024-11-19 14:35:47 +0100

    14:35 19.11.2024

---
+++ Project1/src/Project1/KlausurAufgabe.java
@@ -0,0 +1,18 @@
+package Project1;
+
+public class KlausurAufgabe {
+	
+	public static void main(String[] args) {
+		int rows = 4;
+		int columns = 1;
+		int num = 1;
+		for(int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				System.out.print(num + " ");
+				num++;
+			}
+			columns++;
+			System.out.println();
+		}
+	}
+}
---
+++ Project1/src/Project1/KlausurAufgabe2.java
@@ -0,0 +1,12 @@
+package Project1;
+
+public class KlausurAufgabe2 {
+
+	public static void main(String[] args) {
+		int[] numbers = {0, 4, 16, -3, 99};
+		int index = 3;
+		int value = numbers[index];
+		System.out.println((value >= 0) ? value : "Negativer Wert");
+	}
+
+}
