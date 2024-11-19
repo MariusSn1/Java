86e93476a5b5d0b5bc17d6ca0b1e676c6b24ed05
sneideraitism <sneideraitism@IAL13855.ial.intra>
2024-11-19 14:45:24 +0100

    14:45 19.11.2024

--- Project1/src/Project1/KlausurAufgabe.java
+++ Project1/src/Project1/KlausurAufgabe.java
@@ -5,7 +5,7 @@ public class KlausurAufgabe {
 	public static void main(String[] args) {
 		int rows = 4;
 		int columns = 1;
-		int num = 1;
+		int num;
 		for(int i = 0; i < rows; i++) {
 			num = 1;
 			for (int j = 0; j < columns; j++) {
