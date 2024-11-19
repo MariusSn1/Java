196bf2d2d7e81668e34e4c0005408f9777be2cb2
sneideraitism <sneideraitism@IAL13855.ial.intra>
2024-11-19 11:47:37 +0100

    11:47 19.11.2024

--- Project1/src/Project1/Factorial.java
+++ Project1/src/Project1/Factorial.java
@@ -5,12 +5,11 @@ public class Factorial {
 	public static void main(String[] args) {
 		//
 		int input = 11;		
-		int output = factorial(input);
-		System.out.println(output);
+		System.out.println(factorial(input));
 	}
 	
-	public static int factorial (int num) {
-		int result = 1;
+	public static long factorial (int num) {
+		long result = 1;
 		for (int i = 1; i <= num; i++) {
 			result = result * i;
 		}
